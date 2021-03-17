0c07e2b0a4fe654882d991b7a14298cd657880ec
Sascha Jung <sascha.jung@tu-dortmund.de>
2021-03-17 14:43:55 +0100

    generation of 3d confs only if necessary and top aligned conformer is now returned

--- shapesearch.py
+++ shapesearch.py
@@ -5,20 +5,27 @@ from rdkit.Chem import rdMolAlign
 from rdkit.Chem.Pharm2D import Gobbi_Pharm2D, Generate
 
 
-def gen_query_conf(query, num_confs, seed):
+def gen_query_conf_pfp(query, num_confs, seed):
+    factory = Gobbi_Pharm2D.factory
     for i in query:
-        factory = Gobbi_Pharm2D.factory
-        mol_H = Chem.AddHs(query[i]["mol"])
-        Chem.EmbedMultipleConfs(mol_H, numConfs=num_confs, randomSeed=seed, ETversion=2, numThreads=0)
-        try:
-            Chem.MMFFOptimizeMoleculeConfs(mol_H, numThreads=0)
-        except:
-            pass
-        mol_3D = Chem.RemoveHs(mol_H)
-        query[i]["confs"] = mol_3D
-        query[i]["param"] = Chem.MMFFGetMoleculeProperties(mol_3D)
-        fp_mol = Generate.Gen2DFingerprint(mol_3D, factory, dMat=Chem.Get3DDistanceMatrix(mol_3D, confId=0))
-        query[i]["fp_shape"] = fp_mol
+        if query[i]["mol"].GetNumConformers() == 0:
+            mol_H = Chem.AddHs(query[i]["mol"])
+            Chem.EmbedMultipleConfs(mol_H, numConfs=num_confs, randomSeed=seed, ETversion=2, numThreads=0)
+            try:
+                Chem.MMFFOptimizeMoleculeConfs(mol_H, numThreads=0)
+            except:
+                pass
+            mol_3D = Chem.RemoveHs(mol_H)
+            query[i]["confs"] = mol_3D
+            query[i]["param"] = Chem.MMFFGetMoleculeProperties(mol_3D)
+            fp_mol = Generate.Gen2DFingerprint(mol_3D, factory, dMat=Chem.Get3DDistanceMatrix(mol_3D, confId=0))
+            query[i]["fp_shape"] = fp_mol
+        else:
+            mol_3D = query[i]["mol"]
+            query[i]["confs"] = mol_3D
+            query[i]["param"] = Chem.MMFFGetMoleculeProperties(mol_3D)
+            fp_mol = Generate.Gen2DFingerprint(mol_3D, factory, dMat=Chem.Get3DDistanceMatrix(mol_3D, confId=0))
+            query[i]["fp_shape"] = fp_mol
 
 
 
@@ -44,7 +51,7 @@ def shape_pfp_tani(mols, query):
             max_shape_simis = max(shape_simis)[0]
             max_pharm_simis = max(pharm_simis)[0]
             combo = max(shape_simis)[0] + max(pharm_simis)[0]
-            score.append((combo, max_shape_simis, max_pharm_simis, i, j, max(shape_simis)[1]))
+            score.append((combo, max_shape_simis, max_pharm_simis, i, j, max(shape_simis)[1], mols[j]["confs"]))
             print(counter)
             counter += 1
     print(max(score))
@@ -56,7 +63,8 @@ def shape_pfp_tani_mp(db_mol, i, query_mol, query_fp, j):
     factory = Gobbi_Pharm2D.factory
     algns = rdMolAlign.GetO3AForProbeConfs(db_mol, query_mol,
                                            prbPyMMFFMolProperties=Chem.MMFFGetMoleculeProperties(db_mol),
-                                           refPyMMFFMolProperties=Chem.MMFFGetMoleculeProperties(query_mol), refCid=0, numThreads=0)
+                                           refPyMMFFMolProperties=Chem.MMFFGetMoleculeProperties(query_mol),
+                                           refCid=0, numThreads=0)
     shape_simis = []
     pharm_simis = []
     for k in range(len(algns)):
@@ -68,21 +76,34 @@ def shape_pfp_tani_mp(db_mol, i, query_mol, query_fp, j):
     max_shape_simis = max(shape_simis)[0]
     max_pharm_simis = max(pharm_simis)[0]
     combo = max(shape_simis)[0] + max(pharm_simis)[0]
-    return (combo, max_shape_simis, max_pharm_simis, j, i, max(shape_simis)[1])
+    return (combo, max_shape_simis, max_pharm_simis, j, i, max(shape_simis)[1], db_mol)
 
 
 def gen_query_conf_mp(mol, i, num_confs, seed):
     factory = Gobbi_Pharm2D.factory
-    mol_H = Chem.AddHs(mol)
-    Chem.EmbedMultipleConfs(mol_H, numConfs=num_confs, randomSeed=seed, ETversion=2, numThreads=0)
-    try:
-        Chem.MMFFOptimizeMoleculeConfs(mol_H, numThreads=0)
-    except:
-        pass
-    mol_3D = Chem.RemoveHs(mol_H)
-    # query[i]["confs"] = mol_3D
+    if mol.GetNumConformers() == 0:
+        params = Chem.ETKDGv3()
+        params.useSmallRingTorsions = True
+        params.useMacrocycleTorsions = True
+        #params.pruneRmsThresh = -1.0
+        params.numThreads = 0
+        params.randomSeed = seed
+        mol_H = Chem.AddHs(mol)
+        Chem.EmbedMultipleConfs(mol_H, numConfs=num_confs, params=params)
+        # Chem.EmbedMultipleConfs(mol_H, numConfs=num_confs, randomSeed=seed, ETversion=2, numThreads=0)
+        try:
+            Chem.MMFFOptimizeMoleculeConfs(mol_H, numThreads=0, maxIters=2000)
+        except:
+            pass
+        mol_3D = Chem.RemoveHs(mol_H)
+        fp_mol = Generate.Gen2DFingerprint(mol_3D, factory, dMat=Chem.Get3DDistanceMatrix(mol_3D, confId=0))
+    else:
+        print("No confs")
+        mol_3D = mol
+        fp_mol = Generate.Gen2DFingerprint(mol_3D, factory, dMat=Chem.Get3DDistanceMatrix(mol_3D, confId=0))
+    #query[i]["confs"] = mol_3D
     # query[i]["param"] = Chem.MMFFGetMoleculeProperties(mol_3D)
     # mol_params = Chem.MMFFGetMoleculeProperties(mol_3D)
     # print(type(mol_params))
-    fp_mol = Generate.Gen2DFingerprint(mol_3D, factory, dMat=Chem.Get3DDistanceMatrix(mol_3D, confId=0))
+    # fp_mol = Generate.Gen2DFingerprint(mol_3D, factory, dMat=Chem.Get3DDistanceMatrix(mol_3D, confId=0))
     return (i, mol_3D, fp_mol)#mol_params)#, fp_mol
